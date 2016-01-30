c858a159a33768be3cc0130287fceacfafce31b1
Christian Bomprezzi <chris.bomprezzi@gmail.com>
2016-01-30 12:14:55 +0100

    Add ctor QTE

---
+++ AShamanJourney/AShamanJourney/Ritual/QuickTimeEvent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Aiv.Engine;
+using Aiv.Fast2D;
+
+namespace AShamanJourney.Ritual
+{
+    class QuickTimeEvent
+    {
+        List<char> key;
+        private int speed;
+
+        public QuickTimeEvent()
+        {
+            speed = 100;
+
+            key = new List<char>();
+
+            Utils.RandomKeys(5 + (int)GameManager.GlobalTimer / 60, new KeyCode[] { KeyCode.W, KeyCode.A, KeyCode.S, KeyCode.D, KeyCode.Up, KeyCode.Right, KeyCode.Right, KeyCode.Left });
+        }
+
+        public void Update()
+        {
+            
+        }
+    }
+}
