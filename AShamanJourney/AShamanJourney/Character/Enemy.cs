e39ed2c11827b8f256693e13a4384672167e242f
Christian Bomprezzi <chris.bomprezzi@gmail.com>
2016-01-30 18:29:08 +0100

    Enemy ed EnemyInfo. NO TARGET

--- AShamanJourney/AShamanJourney/Character/Enemy.cs
+++ AShamanJourney/AShamanJourney/Character/Enemy.cs
@@ -3,13 +3,71 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Aiv.Engine;
+using OpenTK;
 
 namespace AShamanJourney
 {
     public class Enemy : Character
     {
+        private Vector2 lastPosition;
+        private Vector2 target;
+        public float radius;
+
         public Enemy(string name, int width, int height, Dictionary<string, float> levelUpModifiers) : base(name, width, height, levelUpModifiers)
         {
+
+        }
+
+        public override void Start()
+        {
+            base.Start();
+            LoadAnimations();
+        }
+
+        public override void Update()
+        {
+            base.Update();
+            if (GameManager.MainWindow != "game") return;
+
+            var player = (Player)Engine.Objects["player"];
+            Vector2 playerIndex = new Vector2(player.X, player.Y);
+            Vector2 enemyIndex = new Vector2(X, Y);
+
+            Vector2 diff = playerIndex - enemyIndex;
+
+            if (diff.Length < radius * 2)
+            {
+                diff.Normalize();
+                diff = diff * Stats.Speed * DeltaTime;
+                enemyIndex.X += diff.X;
+                enemyIndex.Y += diff.Y;
+            }
+
+            ManageCollisions(player);
+        }
+
+        private void LoadAnimations()
+        {
+            // TODO: add animations
+            CurrentSprite = (SpriteAsset)Engine.GetAsset("enemy");
+        }
+
+        private void ManageCollisions(Player player)
+        {
+            foreach (var collision in CheckCollisions())
+            {
+                if (collision.Other.Name == "player")
+                    DoDamage(player);
+                //else
+                //{
+                //    Vector2 diff = Vector2.Subtract(new Vector2(X, Y), new Vector2(collision.Other.X, collision.Other.Y));
+                //    if (diff.X > 0 && diff.Y > 0)
+                //    {
+                //        target = new Vector2(target.X - 20, target.Y);
+                //    }
+                //}
+            }
         }
     }
 }
--- AShamanJourney/AShamanJourney/Character/EnemyInfo.cs
+++ AShamanJourney/AShamanJourney/Character/EnemyInfo.cs
@@ -13,15 +13,38 @@ namespace AShamanJourney
         public Enemy wolf;
         public Enemy boar;
 
+        private static readonly Dictionary<string, float> LevelUpModifiers = new Dictionary<string, float>()
+        {
+            { "attack", 0.3f }, { "maxHp", 0.2f }, { "xpReward", 0.4f },
+            { "speed", 0.1f }, { "xpForNextLevel", 0.1f }
+        };
+
+
         public void Enemies(Engine engine)
         {
-            bear = new Enemy();
-            bear.Stats.Attack = 30f;
+            bear = new Enemy("bear", 70, 40, LevelUpModifiers);
+            bear.Stats.Hp = 150f;
+            bear.Stats.MaxHp = 200f;
+            bear.Stats.Speed = 600f;
+            bear.Stats.Attack = 90f;
+            bear.Stats.XpForNextLevel = 105;
+            bear.radius = 100f;
 
-            
+            wolf = new Enemy("wolf", 50, 20, LevelUpModifiers);
+            wolf.Stats.Hp = 90f;
+            wolf.Stats.MaxHp = 180f;
+            wolf.Stats.Speed = 400f;
+            wolf.Stats.Attack = 100f;
+            wolf.Stats.XpForNextLevel = 60;
+            wolf.radius = 100f;
 
-            wolf = new Enemy();
-            boar = new Enemy();
+            boar = new Enemy("boar", 60, 30, LevelUpModifiers);
+            boar.Stats.Hp = 200f;
+            boar.Stats.MaxHp = 150f;
+            boar.Stats.Speed = 450f;
+            boar.Stats.Attack = 140f;
+            boar.Stats.XpForNextLevel = 90;
+            boar.radius = 100f;
         }
     }
 }
