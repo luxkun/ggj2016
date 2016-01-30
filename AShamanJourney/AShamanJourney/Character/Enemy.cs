1d02718a266431151a2b271cda216ee36138e0b7
MattiaPezzanoAiv <mattiapezzano7@gmail.com>
2016-01-30 21:44:50 +0100

    Add enemy movement and dodge obstacles(i hope)

--- AShamanJourney/AShamanJourney/Character/Enemy.cs
+++ AShamanJourney/AShamanJourney/Character/Enemy.cs
@@ -13,16 +13,20 @@ namespace AShamanJourney
         private Vector2 lastPosition;
         private Vector2 target;
         public float radius;
+        private bool goingToPlayer;
 
         public Enemy(string name, int width, int height, Dictionary<string, float> levelUpModifiers) : base(name, width, height, levelUpModifiers)
         {
-
+            goingToPlayer = true;
         }
 
         public override void Start()
         {
             base.Start();
             LoadAnimations();
+            var player = (Player)Engine.Objects["player"];
+            Vector2 playerIndex = new Vector2(player.X, player.Y);
+            target = playerIndex;
         }
 
         public override void Update()
@@ -33,8 +37,11 @@ namespace AShamanJourney
             var player = (Player)Engine.Objects["player"];
             Vector2 playerIndex = new Vector2(player.X, player.Y);
             Vector2 enemyIndex = new Vector2(X, Y);
+            
+            if(goingToPlayer) // check if the enemy is going to player, after refresh the current player position
+                target = playerIndex;
 
-            Vector2 diff = playerIndex - enemyIndex;
+            Vector2 diff = target - enemyIndex;
 
             if (diff.Length < radius * 2)
             {
@@ -42,6 +49,12 @@ namespace AShamanJourney
                 diff = diff * Stats.Speed * DeltaTime;
                 enemyIndex.X += diff.X;
                 enemyIndex.Y += diff.Y;
+            }
+            if (diff.Length < 15)  //if the enemy is near target
+            {
+                X = target.X;
+                Y = target.Y;
+                target = playerIndex;
             }
 
             ManageCollisions(player);
@@ -59,15 +72,34 @@ namespace AShamanJourney
             {
                 if (collision.Other.Name == "player")
                     DoDamage(player);
-                //else
-                //{
-                //    Vector2 diff = Vector2.Subtract(new Vector2(X, Y), new Vector2(collision.Other.X, collision.Other.Y));
-                //    if (diff.X > 0 && diff.Y > 0)
-                //    {
-                //        target = new Vector2(target.X - 20, target.Y);
-                //    }
-                //}
+                else
+                {
+                    Vector2 diff = Vector2.Subtract(new Vector2(X, Y), new Vector2(collision.Other.X, collision.Other.Y));
+                    if ((diff.X > 0 && diff.Y > 0)) 
+                        if (X + Width <= collision.Other.X ) //check right collision
+                            target = new Vector2(target.X + collision.Other.HitBoxes[collision.Other.Name].Width, target.Y);
+                        else //under collision
+                            target = new Vector2(target.X, target.Y + collision.Other.HitBoxes[collision.Other.Name].Height);
+                    if((diff.X > 0 && diff.Y < 0))
+                        if (X + Width <= collision.Other.X) //check right collision
+                            target = new Vector2(target.X + collision.Other.HitBoxes[collision.Other.Name].Width, target.Y);
+                        else //under collision
+                            target = new Vector2(target.X, target.Y - collision.Other.HitBoxes[collision.Other.Name].Height);
+
+
+                    if ((diff.X < 0 && diff.Y > 0))
+                        if(X >= collision.Other.X + collision.Other.HitBoxes[collision.Other.Name].Width)
+                            target = new Vector2(target.X - collision.Other.HitBoxes[collision.Other.Name].Width, target.Y);
+                        else
+                            target = new Vector2(target.X, target.Y - collision.Other.HitBoxes[collision.Other.Name].Height);
+                    if ((diff.X < 0 && diff.Y < 0))
+                        if (X >= collision.Other.X + collision.Other.HitBoxes[collision.Other.Name].Width)
+                            target = new Vector2(target.X - collision.Other.HitBoxes[collision.Other.Name].Width, target.Y);
+                        else
+                            target = new Vector2(target.X, target.Y + collision.Other.HitBoxes[collision.Other.Name].Height);
+                }
             }
+            
         }
     }
 }
