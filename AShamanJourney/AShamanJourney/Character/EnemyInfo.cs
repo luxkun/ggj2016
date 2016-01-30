3f74d1cb72c1eaf87ba2de7f79424d696ab8dc71
Christian Bomprezzi <chris.bomprezzi@gmail.com>
2016-01-30 15:56:43 +0100

    pl

---
+++ AShamanJourney/AShamanJourney/Character/EnemyInfo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Aiv.Engine;
+
+namespace AShamanJourney.Character
+{
+    class EnemyInfo
+    {
+        public Enemy bear;
+        public Enemy wolf;
+        public Enemy boar;
+
+        public void Enemies(Engine engine)
+        {
+            bear = new Enemy();
+            bear.Stats.Attack = 30f;
+
+            
+
+            wolf = new Enemy();
+            boar = new Enemy();
+        }
+    }
+}
